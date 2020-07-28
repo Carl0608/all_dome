b7ed9b054cd8dbdb05cfdffa3d261b06151c66c5
kanghao <carl0608@126.com>
2020-07-28 20:55:15 +0800

    提示音

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # demo -- 各种插件试用
 
+"xlsx-style"报错：在\node_modules\xlsx-style\dist\cpexcel.js 807行 的 var cpt = require(’./cpt’ + ‘able’); 改成 var cpt = cptable;
+
 1.日历demo 使用FullCalendar 路由 table_list  使用vue-full-calendar 路由 full
 2.百度地图  使用vue-baidu-map 路由 map 
 3.高德地图  使用vue-amap 路由 gaode
-4.视频直播类 使用vue-video-player 路由HelloWorld
+4.视频直播类 使用vue-video-player 路由HelloWorld 
 
 ## Project setup
 ```
--- src/components/mock.vue
+++ src/components/mock.vue
@@ -15,6 +15,11 @@
       >
       Your browser does not support the video tag.
     </video> -->
+    <button @click="add">点击提示音</button>
+    <audio
+      id="audio"
+      src="http://mp3.9ku.com/mp3/610/609411.mp3"
+    />
   </div>
 </template>
 
@@ -27,25 +32,35 @@ export default {
   created() {
     this.getData();
   },
+  watch: {
+    errCount: function () {
+      this.aplayAudio();
+    },
+  },
   methods: {
+    /* 提示音 */
+    add() {
+      const audio = document.getElementById("audio");
+      audio.play();
+    },
     getRandom() {
       this.mockData = Mock.mock({
         //生成数据规则
         "list|1-10": [
           {
-            "id|+1": 1
-          }
-        ]
+            "id|+1": 1,
+          },
+        ],
       });
       document.body.innerHTML +=
         "<pre>" + JSON.stringify(this.mockData, null, 4) + "</pre>"; // 展示到页面中
     },
     getData() {
-      this.$axios.post("/list").then(res => {
+      this.$axios.post("/list").then((res) => {
         console.log(res);
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
